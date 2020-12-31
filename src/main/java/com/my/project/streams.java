5206ef49d7b35c80996192b5d7258faae502c1a4
rs <tonewbeginning21@gmail.com>
2020-12-30 22:08:45 -0500

    streams

---
+++ src/main/java/com/my/project/streams.java
@@ -0,0 +1,23 @@
+package com.my.project;
+//https://www.tutorialspoint.com/java8/java8_streams.htm
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class streams {
+    public static void main(String[] args) {
+        // filter
+        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+        List<String> filtered = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.toList());
+        filtered.toString(filtered.forEach(System.out::println);
+
+        // stats
+        List numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+        IntSummaryStatistics stats = numbers.stream().mapToInt((x) -> x).summaryStatistics();
+        System.out.println("Highest number in List : " + stats.getMax());
+        System.out.println("Lowest number in List : " + stats.getMin());
+        System.out.println("Sum of all numbers : " + stats.getSum());
+        System.out.println("Average of all numbers : " + stats.getAverage());
+    }
+}
